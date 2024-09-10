934fc91558d35e7e9262a1b6a5f15669f207be19
piggy <208455520@qq.com>
2024-09-10 16:59:32 +0800

    新增显示URL参数的功能
    在`IndexController`中添加了新的`showParam`方法，以接收URL参数并将其显示在`index.html`页面上。该功能允许通过查询参数传递值，并在页面上动态显示该值。

--- src/main/java/com/piggy/controller/IndexController.java
+++ src/main/java/com/piggy/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.piggy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -23,6 +24,22 @@ public class IndexController {
         Map<String, Object> model = modelAndView.getModel();
         // 添加数据
         model.put("msg", "hello world!");
+
+        return modelAndView;
+    }
+
+    /**
+     * 请求
+     * http://localhost:8081/showParam?urlParam=123456
+     */
+    @RequestMapping("/showParam")
+    public ModelAndView showParam(@RequestParam String urlParam) {
+        // 创建ModelAndView对象, 指定页面名称
+        ModelAndView modelAndView = new ModelAndView("index");
+        // 获取Model
+        Map<String, Object> model = modelAndView.getModel();
+        // 添加数据
+        model.put("urlParam", urlParam);
 
         return modelAndView;
     }
