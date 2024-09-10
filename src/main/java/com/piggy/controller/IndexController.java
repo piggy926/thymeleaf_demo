6bd8e773db56f0de7655caa4e1c3418f0920a1bf
piggy <208455520@qq.com>
2024-09-10 16:53:30 +0800

    实现简单的消息传递,通过ModelAndView返回动态数据，展示在前端。

--- src/main/java/com/piggy/controller/IndexController.java
+++ src/main/java/com/piggy/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.piggy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -10,5 +13,17 @@ public class IndexController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        // 创建ModelAndView对象, 指定页面名称
+        ModelAndView modelAndView = new ModelAndView("index");
+        // 获取Model
+        Map<String, Object> model = modelAndView.getModel();
+        // 添加数据
+        model.put("msg", "hello world!");
+
+        return modelAndView;
     }
 }
